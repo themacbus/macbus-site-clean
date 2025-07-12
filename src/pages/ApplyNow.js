3f474c618c01fe79773fded510e411fe34301a0f
Henry McFarlin <henrymcfarlin@Henrys-MacBook-Pro.local>
2025-07-12 13:29:18 -0400

    Connected ApplyNow form to Formspree

--- src/pages/ApplyNow.js
+++ src/pages/ApplyNow.js
@@ -33,7 +33,7 @@ export default function ApplyNow() {
     }
 
     try {
-const response = await fetch("https://formspree.io/f/YOUR_FORM_ID", {
+const response = await fetch("hhttps://formspree.io/f/mzzvppzb", {
         method: "POST",
         body,
         headers: {
